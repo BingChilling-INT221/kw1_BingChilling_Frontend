985da6e0accd13d24104b9cf0f45b909e8aacf6a
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-05-02 18:02:59 +0700

    switch

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const router = createRouter({
     {
       path: '/admin/announcement/:id',
       name: 'Tablesdetail',
-      component: AnnouceDetail
+      component: Tablesdetail
     },
     {
       path: '/',
