09f3cef79bdfb778edbe30808038490d7dafc1a7
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-05-02 19:49:53 +0700

    format code

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <script setup>
-import Announcement from './components/Announcement.vue';
+import Announcement from './components/Announcement.vue';</script>
 
-
-</script>
- 
 <template>
 
-<div class="w-full h-screen bg-[#1D2040]">
+  <div class="w-full h-screen bg-[#1D2040]">
     <Announcement/>
-<div class=" w-full h-auto bg-[#1D2040] bg-cover mt-10">
-    <RouterView/>
-</div>
+    <div class=" w-full h-auto bg-[#1D2040] bg-cover mt-10">
+      <RouterView/>
+    </div>
 
-</div>
+  </div>
 
 
 </template>
- 
-<style scoped>
 
+<style scoped>
 
 
 </style>
--- src/views/Announcedetail.vue
+++ src/views/Announcedetail.vue
@@ -1,35 +1,35 @@
 <script setup>
-import { onMounted, ref } from "vue";
-import { useRoute,useRouter } from "vue-router";
+import {onMounted, ref} from "vue";
+import {useRoute, useRouter} from "vue-router";
+
 const queryAnnounce = ref({});
 const route = useRoute();
 const router = useRouter();
 onMounted(async () => {
   try {
     const result = await fetch(
-      `${import.meta.env.VITE_BASE_URL}/${route.params.id}`
+        `${import.meta.env.VITE_BASE_URL}/${route.params.id}`
     );
     if (result.status === 200) {
       const response = await result.json();
       queryAnnounce.value = response;
       console.log(queryAnnounce.value);
-    }
-    else if (result.status === 404 || result.status === 400) {
+    } else if (result.status === 404 || result.status === 400) {
       console.log("404")
       alert("The request page is not available")
       router.push(`/admin/announcement/`)
-    }
-    else {
+    } else {
       console.log("Something went wrong")
     }
   } catch (err) {
     alert("The request page is not available")
-      router.push(`/admin/announcement/`)
+    router.push(`/admin/announcement/`)
   }
 });
 
 const date = new Date();
 const UTC = date.getTimezoneOffset();
+
 function toHoursAndMinutes(totalMinutes) {
   const negetive = totalMinutes > 0 ? "UTC-" : "UTC+";
   if (totalMinutes < 0) {
@@ -39,6 +39,7 @@ function toHoursAndMinutes(totalMinutes) {
   const minutes = totalMinutes % 60;
   return `${negetive + padToTwoDigits(hours)}:${padToTwoDigits(minutes)}`;
 }
+
 function padToTwoDigits(num) {
   return num.toString().padStart(2, "0");
 }
@@ -54,10 +55,10 @@ const changeTime = (time) => {
     year: "numeric",
   };
   return `${newDate.toLocaleDateString("en-GB", options).replace(/[,]/gi, '') +
-    ", " +
-    newDate.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", hour12: false })
+  ", " +
+  newDate.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit", hour12: false})
 
-    }`;
+  }`;
 };
 </script>
 
@@ -66,13 +67,13 @@ const changeTime = (time) => {
     <div class="flex h-auto m-auto">
       <div class="flex w-full pl-12 space-x-2">
         <div class="w-6 pt-3">
-            <button class="ann-button" @click="$router.back()">Back</button>
+          <button class="ann-button" @click="$router.back()">Back</button>
         </div>
       </div>
 
       <div class="flex justify-end w-full pt-2 ">
         <p class="pr-2 text-lg">{{ toHoursAndMinutes(UTC) }}</p>
-        <img src="../assets/1845948.png" alt="" class="h-6 pr-1" />
+        <img alt="" class="h-6 pr-1" src="../assets/1845948.png"/>
       </div>
     </div>
     <div class="pt-5">
@@ -84,12 +85,12 @@ const changeTime = (time) => {
           </p>
           <div class="pt-1 pr-5 ann-display">
             <a
-              href="#"
-              class="flex justify-center w-20 p-2 text-white bg-green-500 rounded-lg"
-              :class="
+                :class="
               queryAnnounce.announcementDisplay === 'Y' ? 'bg-green-500' : 'bg-red-500'
             "
-              >{{ queryAnnounce.announcementDisplay }}</a
+                class="flex justify-center w-20 p-2 text-white bg-green-500 rounded-lg"
+                href="#"
+            >{{ queryAnnounce.announcementDisplay }}</a
             >
           </div>
         </div>
@@ -102,18 +103,18 @@ const changeTime = (time) => {
           </p>
         </div>
         <div class="pt-5 font-bold text-black border-2 rounded-lg">
-            <p class="pl-5">Description:</p>
-            <div class="h-auto">
-                <p class="p-5 pl-5 ann-description">{{ queryAnnounce.announcementDescription }}</p>
-            </div>
+          <p class="pl-5">Description:</p>
+          <div class="h-auto">
+            <p class="p-5 pl-5 ann-description">{{ queryAnnounce.announcementDescription }}</p>
+          </div>
         </div>
         <div class="pt-5 ann-category">
-                <a
-          href="#"
-          class="text-white bg-[#628FB8] px-5 text-sm rounded-lg py-1"
+          <a
+              class="text-white bg-[#628FB8] px-5 text-sm rounded-lg py-1"
+              href="#"
           >{{ queryAnnounce.announcementCategory }}</a
-        >
-            </div>
+          >
+        </div>
       </div>
     </div>
   </div>
