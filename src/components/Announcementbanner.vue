9cb41b0f3660ccab1f9c1e72580e76dbd8e6fe42
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-05-02 22:39:12 +0700

    fix theme

---
+++ src/components/Announcementbanner.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="font-mono">
+    <div class="w-full h-full">
+      <div class="">
+        <div class="flex w-full pl-24 space-x-16">
+          <div class="flex items-center w-30 justify-items-center">
+            <img alt="" class="w-full h-auto" src="../assets/boost.png"/>
+          </div>
+          <div class="flex items-center justify-items-center">
+            <p class="flex py-2 space-x-16 font-bold text-white text-7xl ">SIT Announcement System (SAS)</p>
+          </div>
+        </div>
+      </div>
+      <div>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
