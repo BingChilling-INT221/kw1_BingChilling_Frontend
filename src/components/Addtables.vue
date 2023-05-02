ebcece868a233b5a1d9694c3ea986e7ccb0099a7
Lahirerr <m_tanachart@hotmail.com>
2023-05-02 22:40:26 +0700

    mo hai push

--- src/components/Addtables.vue
+++ src/components/Addtables.vue
@@ -1,50 +1,104 @@
 <script setup>
+import { onMounted, ref, computed } from "vue";
+
+const announcementTitle = ref("")
+const categoryId = ref(Number)
+const announcementDescription = ref("")
+const publishDate =ref("")
+const publishTime = ref("")
+const closeDate = ref("")
+const closeTime = ref("")
+const announcementDisplay = ref("N")
+
+const data = computed(() => {
+  return {
+    announcementTitle: announcementTitle.value,
+    categoryId: categoryId.value,
+    announcementDescription: announcementDescription.value,
+    publishDate: publishDate.value + " " + publishTime.value,
+    publishTime: closeTime.value + " " + closeDate.value,
+    announcementDisplay: announcementDisplay.value ? "Y" : "N"
+  }
+})
+
+onMounted(async () => {
+  try {
+    const result = await fetch(
+      `${import.meta.env.VITE_BASE_URL}`
+    );
+    if (result.status === 200) {
+      const response = await result.json();
+      announce.value = response;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 </script>
- 
+
 <template>
-    <div class="p-2 ">
-<div class="border-2 m-auto p-4 rounded-lg">
-<p class="text-black font-bold text-3xl">Announcement Detail: </p>
-<form action="">
-    <p class="text-black font-bold py-2">Title</p>
-    <input type="text" class="border-2 rounded-md w-full py-1 ann-title">
-    <p class="text-black font-bold py-2">Category</p>
-    <select class="w-3/12 shadow-md shadow-slate-300 ann-category">
-    <option value="ทั่วไป">ทั่วไป</option>
-    <option value="ทุนการศึกษา">ทุนการศึกษา</option>
-    <option value="หางาน">หางาน</option>
-        <option value="ฝึกงาน">ฝึกงาน</option>
-  </select>
-  <p class="text-black font-bold py-2">Description</p>
-  <textarea cols="100" rows="10" class="w-full border-2 rounded-md ann-description"></textarea>
-  <p class="text-black font-bold py-2">Publish Date</p>
-    <div class="flex space-x-4">
-    <input type="text" class="border-2 rounded-md w-1/12 py-1 ann-publish-date" placeholder="  01/05/2023">
-  <input type="text" class="border-2 rounded-md w-1/12 py-1 ann-publish-time" placeholder="  12:30">
-  </div>
-  <p class="text-black font-bold py-2">Close Date</p>
-  <div class="flex space-x-4">
-    <input type="text" class="border-2 rounded-md w-1/12 py-1 ann-close-date" placeholder="  01/05/2023">
-  <input type="text" class="border-2 rounded-md w-1/12 py-1 ann-close-time" placeholder="  12:30">
-  </div>
-  <p class="text-black font-bold py-2">Display</p>
-  <input type="checkbox" value="N">
-  <label class="font-semibold">  Check to show this announcement</label>
+  <div class="p-2">
+    <div class="border-2 m-auto p-4 rounded-lg">
+      <p class="text-white font-bold text-3xl">Announcement Detail:</p>
+      <form action="">
+        <p class="text-white font-bold py-2">Title</p>
+        <input type="text" class="border-2 rounded-md w-full py-1 ann-title" v-model="announcementTitle" />
+        <p class="text-white font-bold py-2">Category</p>
+        <select class="w-3/12 shadow-md shadow-slate-300 ann-category">
+          <option value="ทั่วไป">ทั่วไป</option>
+          <option value="ทุนการศึกษา">ทุนการศึกษา</option>
+          <option value="หางาน">หางาน</option>
+          <option value="ฝึกงาน">ฝึกงาน</option>
+        </select>
+        <p class="text-white font-bold py-2">Description</p>
+        <textarea
+          cols="100"
+          rows="10"
+          class="w-full border-2 rounded-md ann-description"
+        ></textarea>
+        <p class="text-white font-bold py-2">Publish Date</p>
+        <div class="flex space-x-4">
+          <input
+            type="text"
+            class="border-2 rounded-md w-1/12 py-1 ann-publish-date"
+            placeholder="  01/05/2023"
+          />
+          <input
+            type="text"
+            class="border-2 rounded-md w-1/12 py-1 ann-publish-time"
+            placeholder="  12:30"
+          />
+        </div>
+        <p class="text-white font-bold py-2">Close Date</p>
+        <div class="flex space-x-4">
+          <input
+            type="text"
+            class="border-2 rounded-md w-1/12 py-1 ann-close-date"
+            placeholder="  01/05/2023"
+          />
+          <input
+            type="text"
+            class="border-2 rounded-md w-1/12 py-1 ann-close-time"
+            placeholder="  12:30"
+          />
+        </div>
+        <p class="text-white font-bold py-2">Display</p>
+        <input type="checkbox" value="N" />
+        <label class="font-semibold text-white"> Check to show this announcement</label>
 
-  <div class="py-5 flex space-x-2">
-      <button class="ann-button bg-gray-300 px-4 py-1 rounded-md">Submit</button>
-      <button class="ann-button bg-gray-300 px-4 py-1 rounded-md">Cancel</button>
+        <div class="py-5 flex space-x-2">
+          <button class="ann-button bg-gray-300 px-4 py-1 rounded-md">
+            Submit
+          </button>
+          <button class="ann-button bg-gray-300 px-4 py-1 rounded-md">
+            Cancel
+          </button>
+        </div>
+        <p class="text-white">{{ data }}</p>
+      </form>
     </div>
-
-
-</form>
-
-</div>
-    </div>
-
+  </div>
 </template>
- 
-<style scoped>
 
-</style>
+<style scoped></style>
