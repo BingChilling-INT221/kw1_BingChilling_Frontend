b6314e4afacea1702a3e16849092869641ec2235
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-05-02 18:00:58 +0700

    add useRouter

--- src/views/Announcedetail.vue
+++ src/views/Announcedetail.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute,useRouter } from "vue-router";
 const queryAnnounce = ref({});
 const route = useRoute();
-
+const router = useRouter();
 onMounted(async () => {
   try {
     const result = await fetch(
