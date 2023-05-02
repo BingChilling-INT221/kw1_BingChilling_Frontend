3202c01690b045a42d29f8ee57ad5109d24a403d
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-05-02 19:49:56 +0700

    format

--- src/components/Addtables.vue
+++ src/components/Addtables.vue
@@ -1,50 +1,50 @@
 <script setup>
 
 </script>
- 
+
 <template>
-    <div class="p-2 ">
-<div class="border-2 m-auto p-4 rounded-lg">
-<p class="text-black font-bold text-3xl">Announcement Detail: </p>
-<form action="">
-    <p class="text-black font-bold py-2">Title</p>
-    <input type="text" class="border-2 rounded-md w-full py-1 ann-title">
-    <p class="text-black font-bold py-2">Category</p>
-    <select class="w-3/12 shadow-md shadow-slate-300 ann-category">
-    <option value="ทั่วไป">ทั่วไป</option>
-    <option value="ทุนการศึกษา">ทุนการศึกษา</option>
-    <option value="หางาน">หางาน</option>
-        <option value="ฝึกงาน">ฝึกงาน</option>
-  </select>
-  <p class="text-black font-bold py-2">Description</p>
-  <textarea cols="100" rows="10" class="w-full border-2 rounded-md ann-description"></textarea>
-  <p class="text-black font-bold py-2">Publish Date</p>
-    <div class="flex space-x-4">
-    <input type="text" class="border-2 rounded-md w-1/12 py-1 ann-publish-date" placeholder="  01/05/2023">
-  <input type="text" class="border-2 rounded-md w-1/12 py-1 ann-publish-time" placeholder="  12:30">
-  </div>
-  <p class="text-black font-bold py-2">Close Date</p>
-  <div class="flex space-x-4">
-    <input type="text" class="border-2 rounded-md w-1/12 py-1 ann-close-date" placeholder="  01/05/2023">
-  <input type="text" class="border-2 rounded-md w-1/12 py-1 ann-close-time" placeholder="  12:30">
-  </div>
-  <p class="text-black font-bold py-2">Display</p>
-  <input type="checkbox" value="N">
-  <label class="font-semibold">  Check to show this announcement</label>
+  <div class="p-2 ">
+    <div class="border-2 m-auto p-4 rounded-lg">
+      <p class="text-black font-bold text-3xl">Announcement Detail: </p>
+      <form action="">
+        <p class="text-black font-bold py-2">Title</p>
+        <input class="border-2 rounded-md w-full py-1 ann-title" type="text">
+        <p class="text-black font-bold py-2">Category</p>
+        <select class="w-3/12 shadow-md shadow-slate-300 ann-category">
+          <option value="ทั่วไป">ทั่วไป</option>
+          <option value="ทุนการศึกษา">ทุนการศึกษา</option>
+          <option value="หางาน">หางาน</option>
+          <option value="ฝึกงาน">ฝึกงาน</option>
+        </select>
+        <p class="text-black font-bold py-2">Description</p>
+        <textarea class="w-full border-2 rounded-md ann-description" cols="100" rows="10"></textarea>
+        <p class="text-black font-bold py-2">Publish Date</p>
+        <div class="flex space-x-4">
+          <input class="border-2 rounded-md w-1/12 py-1 ann-publish-date" placeholder="  01/05/2023" type="text">
+          <input class="border-2 rounded-md w-1/12 py-1 ann-publish-time" placeholder="  12:30" type="text">
+        </div>
+        <p class="text-black font-bold py-2">Close Date</p>
+        <div class="flex space-x-4">
+          <input class="border-2 rounded-md w-1/12 py-1 ann-close-date" placeholder="  01/05/2023" type="text">
+          <input class="border-2 rounded-md w-1/12 py-1 ann-close-time" placeholder="  12:30" type="text">
+        </div>
+        <p class="text-black font-bold py-2">Display</p>
+        <input type="checkbox" value="N">
+        <label class="font-semibold"> Check to show this announcement</label>
 
-  <div class="py-5 flex space-x-2">
-      <button class="ann-button bg-gray-300 px-4 py-1 rounded-md">Submit</button>
-      <button class="ann-button bg-gray-300 px-4 py-1 rounded-md">Cancel</button>
+        <div class="py-5 flex space-x-2">
+          <button class="ann-button bg-gray-300 px-4 py-1 rounded-md">Submit</button>
+          <button class="ann-button bg-gray-300 px-4 py-1 rounded-md">Cancel</button>
+        </div>
+
+
+      </form>
+
     </div>
-
-
-</form>
-
-</div>
-    </div>
+  </div>
 
 </template>
- 
+
 <style scoped>
 
 </style>
--- src/views/Notfound.vue
+++ src/views/Notfound.vue
@@ -1,14 +1,14 @@
 <script setup>
 
 </script>
- 
+
 <template>
-<div class="bg-white h-auto w-11/12 m-auto p-3">
+  <div class="bg-white h-auto w-11/12 m-auto p-3">
     <p class="text-black"> NOT FOUND!!!</p>
-</div>
+  </div>
 
 </template>
- 
+
 <style scoped>
 
 </style>
