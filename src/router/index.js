ad343e3f9436e7f91f4b9e57c8563b034ec194d5
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-05-03 20:25:08 +0700

    edit name Annoucementdetail

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import homepage from '../views/Homeview.vue'
 import Addtables from '../components/Addtables.vue'
-import announcementdetail from '../views/Announcedetail.vue'
+import announcementdetail from '../views/Announcementdetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
