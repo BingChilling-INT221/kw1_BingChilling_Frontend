6e68fcb522f479843637207f6036efed62b75a89
Lahirerr <m_tanachart@hotmail.com>
2023-05-04 19:23:49 +0700

    nee man arai

--- src/views/addannouncement.vue
+++ src/views/addannouncement.vue
@@ -4,13 +4,15 @@ import { onMounted, ref, computed } from "vue";
 const category = ref([])
 
 const announcementTitle = ref("")
-const categoryName = ref("")
+const categoryId = ref(Number)
 const announcementDescription = ref("")
-const publishDate =ref(null)
-const publishTime = ref(null)
-const closeDate = ref(null)
-const closeTime = ref(null)
+const publishDate =ref("")
+const publishTime = ref("")
+const closeDate = ref("")
+const closeTime = ref("")
 const announcementDisplay = ref("")
+const checkDate = /^(0?[1-9]|[12]\d|3[01])[\/](0?[1-9]|1[0-2])[\/](19|20)\d{2}$/g
+const checkTime = /([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?/g
 
 onMounted( async() => {
   try {
@@ -95,20 +97,37 @@ const checkFeb = (year) => {
   }
   return 28
 }
+
+
 const comeTime = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", hour12: false })
 const comeDate = new Date().toLocaleDateString("en-GB")
+
+
+const isDateTimeFormat = () =>{
+  if(checkDate.test(publishDate.value) || checkDate.test(closeDate.value)){
+    alert("Please enter correct date format")
+    return false
+  }
+  else if(checkTime.test(publishTime.value) || checkTime.test(closeTime.value)){
+    alert("Please enter correct time format")
+    return false
+  }
+  return true
+} 
+
+
 </script>
 
 <template>
   <div class="p-2">
     <div class="border-2 m-auto p-4 rounded-lg">
       <p class="text-white font-bold text-3xl">Announcement Detail:</p>
-      <form action="">
+      <form action="" :onsubmit="isDateTimeFormat()">
         <p class="text-white font-bold py-2">Title</p>
-        <input type="text" class="border-2 rounded-md w-full py-1 ann-title" v-model="announcementTitle" />
+        <input type="text" class="border-2 rounded-md w-full py-1 ann-title" v-model="announcementTitle" required />
         <p class="text-white font-bold py-2">Category</p>
-        <select class="w-3/12 shadow-md shadow-slate-300 ann-category" v-model="categoryName">
-          <option :value="data.categoryName" v-for="(data,index) in category" :key="data.id">{{ data.categoryName }}</option>
+        <select class="w-3/12 shadow-md shadow-slate-300 ann-category" v-model="categoryId" required>
+          <option :value="data.category_Id" v-for="(data,index) in category" :key="data.id">{{ data.categoryName }}</option>
         </select>
         <p class="text-white font-bold py-2">Description</p>
         <textarea
@@ -116,6 +135,7 @@ const comeDate = new Date().toLocaleDateString("en-GB")
           rows="10"
           class="w-full border-2 rounded-md ann-description"
           v-model="announcementDescription"
+          required
         ></textarea>
         <p class="text-white font-bold py-2">Publish Date</p>
         <div class="flex space-x-4">
@@ -124,12 +144,14 @@ const comeDate = new Date().toLocaleDateString("en-GB")
             class="border-2 rounded-md w-1/12 py-1 ann-publish-date"
             :placeholder="'  '+comeDate+''"
             v-model="publishDate"
+            
           />
           <input
             type="text"
             class="border-2 rounded-md w-1/12 py-1 ann-publish-time"
             :placeholder="'  '+comeTime+''"
             v-model="publishTime"
+            
           />
         </div>
         <p class="text-white font-bold py-2">Close Date</p>
@@ -139,16 +161,18 @@ const comeDate = new Date().toLocaleDateString("en-GB")
             class="border-2 rounded-md w-1/12 py-1 ann-close-date"
             :placeholder="'  '+comeDate+''"
             v-model="closeDate"
+            
           />
           <input
             type="text"
             class="border-2 rounded-md w-1/12 py-1 ann-close-time"
             :placeholder="'  '+comeTime+''"
             v-model="closeTime"
+            
           />
         </div>
         <p class="text-white font-bold py-2">Display</p>
-        <input type="checkbox" v-model="announcementDisplay"/>
+        <input type="checkbox" v-model="announcementDisplay" required/>
         <label class="font-semibold text-white"> Check to show this announcement</label>
 
         <div class="py-5 flex space-x-2">
@@ -159,12 +183,14 @@ const comeDate = new Date().toLocaleDateString("en-GB")
             Cancel
           </button>
         </div>
-        <!-- <p class="text-white">{{ data }}</p> -->
+     
+
         <p class="text-white">
-        {{publishDatePlueTime ?? 'null'}}
-        {{ closeDatePlueTime ?? 'null'}}
+        {{publishDatePlueTime }}
+        {{ closeDatePlueTime }}
         {{ announcementDisplay }}
-        {{ categoryName }}
+        {{ categoryId }}
+        {{ announcementDescription }}
       </p>
       </form>
     
