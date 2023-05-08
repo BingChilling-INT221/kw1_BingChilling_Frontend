865c1a558e61e63d4e2463683c9107b9e1e0795c
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-05-08 16:05:20 +0700

    userview

--- src/views/Notfound.vue
+++ src/views/Notfound.vue
@@ -4,10 +4,10 @@
 
 <template>
     <div class="inset-0 bg-white min-h-screen">
-    <div class="bg-white  m-auto p-3 flex flex-row">
+    <div class="bg-white  m-auto p-3 flex flex-col">
         <p class="text-black"> NOT FOUND!!!</p>
-        <button @click="$router.push({name: 'userhomepage'})" class="bg-green-500">Go to Homepage</button>
-        <button @click="$router.push({name: 'adminhomepage'})" class="bg-red-500">Go to Admin Homepage</button>
+        <button @click="$router.push({name: 'userhomepage'})" class="text-green-500">Go to Homepage</button>
+        <button @click="$router.push({name: 'adminhomepage'})" class="text-red-500">Go to Admin Homepage</button>
     </div>
     </div>
 </template>
