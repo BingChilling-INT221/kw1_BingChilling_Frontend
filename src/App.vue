92ef97e62c199766d1c1107fd764740a1ac731d1
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-05-12 02:00:01 +0700

    add font

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <script setup>
-import Announcement from './components/Announcementbanner.vue';</script>
+import Announcement from './components/Announcementbanner.vue';
+import { RouterView } from "vue-router";
+import {computed,watch} from "vue";
+import {useAnnouncerStore} from "@/stores/announcer.js";
+const announcer = useAnnouncerStore();
+const {  setDarkMode } = announcer;
 
+const colorBg = computed(() => {
+    return announcer.darkMode ? 'bg-blackCustom' : 'bg-white'
+})
+const colorText = computed(() => {
+    return announcer.darkMode ? 'text-white' : 'text-black'
+})
+
+</script>
 <template>
 
-    <div class="w-full min-h-screen bg-blackCustom">
-        <Announcement class="bg-purpleCustom1"/>
-        <div class="w-full h-auto ">
-            <RouterView/>
-        </div>
-    </div>
-
+<!--    <div class="w-full min-h-screen bg-blackCustom">-->
+<!--        <Announcement class="bg-purpleCustom1"/>-->
+<!--        <div class="w-full h-auto ">-->
+<!--            <RouterView/>-->
+<!--        </div>-->
+<!--    </div>-->
+    <button @click="setDarkMode">a</button>
+    {{ announcer?.darkMode  }}
+    <RouterView :class="[colorBg,colorText]" ></RouterView>
 
 </template>
 
 <style scoped>
 
+html {
+    scroll-behavior: smooth;
 
+}
 </style>
