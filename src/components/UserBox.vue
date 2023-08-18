616c0b182677c4dd44c92ee5c16cf6bfea984954
Lahirerr <m_tanachart@hotmail.com>
2023-08-18 22:40:00 +0700

    create and move to component UserBox

---
+++ src/components/UserBox.vue
@@ -0,0 +1,71 @@
+<script setup>
+
+</script>
+ 
+<template>
+<div class="flex flex-row justify-between pr-4 ann-item items-center">
+          <div class="flex flex-col space-y-2">
+            <p class="flex justify-center">No.</p>
+            <div class="flex flex-col items-center text-xl space-y-8">
+              <p>10000000</p>
+              
+            </div>
+          </div>
+          <div class="flex flex-col space-y-2">
+            <p class="flex justify-center">Username</p>
+            <div class="flex flex-col items-center text-xl space-y-8">
+              <p>Menoa</p>
+              
+            </div>
+          </div>
+          <div class="flex flex-col space-y-2">
+            <p class="flex justify-center">Name</p>
+            <div class="flex flex-col items-center text-xl space-y-8">
+              <p>Menoa Belluchi</p>
+              
+            </div>
+          </div>
+          <div class="flex flex-col space-y-2">
+            <p class="flex justify-center">Email</p>
+            <div class="flex flex-col items-center text-xl space-y-8">
+              <p>Menoa@hotmail.com</p>
+              
+            </div>
+          </div>
+          <div class="flex flex-col space-y-2">
+            <p class="flex justify-center">Role</p>
+            <div class="flex flex-col items-center text-xl space-y-8">
+              <p>Admin</p>
+              
+            </div>
+          </div>
+          <div class="flex flex-col space-y-2">
+            <p class="flex justify-center">Created On</p>
+            <div class="flex flex-col items-center text-xl space-y-8">
+              <p>15 Aug 2023 08:00</p>
+              
+            </div>
+          </div>
+          <div class="flex flex-col space-y-2">
+            <p class="flex justify-center">Updated On</p>
+            <div class="flex flex-col items-center text-xl space-y-8">
+              <p>15 Aug 2023 08:00</p>
+              
+            </div>
+          </div>
+          <div class="flex flex-col space-y-2">
+            <p class="flex justify-center">Action</p>
+            <div class="flex flex-col items-center text-xl">
+              <div class="flex flex-row space-x-3">
+                <button class="px-4 py-1 rounded-md bg-gray-50 dark:bg-gray-700">edit</button>
+                <button class="px-4 py- rounded-md bg-gray-50 dark:bg-gray-700 ">delete</button>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+</template>
+ 
+<style scoped>
+
+</style>
--- src/views/admin/UsersPage.vue
+++ src/views/admin/UsersPage.vue
@@ -3,6 +3,7 @@ import { provide } from "vue";
 import format_page from "@/components/format_page.vue";
 import dateTimeBox from "../../components/DateTimeBox.vue";
 import timeZoneBox from "../../components/TimeZoneBox.vue";
+import userBox from "../../components/UserBox.vue";
 
 const datetime = new Intl.DateTimeFormat("en-GB", {
   dateStyle: "medium",
@@ -16,7 +17,7 @@ provide(/* key */ "role", /* value */ "admin");
 <template>
   <div class="w-full">
     <format_page>
-      <div class="flex flex-col space-y-2">
+      <div class="flex flex-col space-y-">
         <div class="flex flex-row w-full">
           <div class="flex flex-row w-4/5">
             <dateTimeBox :time="datetime" class="text-sm"></dateTimeBox>
@@ -30,70 +31,7 @@ provide(/* key */ "role", /* value */ "admin");
             </button>
           </div>
         </div>
-        <div class="flex flex-row justify-between pr-4 ann-item items-center">
-          <div class="flex flex-col space-y-2">
-            <p class="flex justify-center">No.</p>
-            <div class="flex flex-col items-center text-xl space-y-8">
-              <p>10000000</p>
-              <p>20000000</p>
-            </div>
-          </div>
-          <div class="flex flex-col space-y-2">
-            <p class="flex justify-center">Username</p>
-            <div class="flex flex-col items-center text-xl space-y-8">
-              <p>Menoa</p>
-              <p>Bell</p>
-            </div>
-          </div>
-          <div class="flex flex-col space-y-2">
-            <p class="flex justify-center">Name</p>
-            <div class="flex flex-col items-center text-xl space-y-8">
-              <p>Menoa Belluchi</p>
-              <p>Bell Bebebop</p>
-            </div>
-          </div>
-          <div class="flex flex-col space-y-2">
-            <p class="flex justify-center">Email</p>
-            <div class="flex flex-col items-center text-xl space-y-8">
-              <p>Menoa@hotmail.com</p>
-              <p>Bell@hotmail.com</p>
-            </div>
-          </div>
-          <div class="flex flex-col space-y-2">
-            <p class="flex justify-center">Role</p>
-            <div class="flex flex-col items-center text-xl space-y-8">
-              <p>Admin</p>
-              <p>user</p>
-            </div>
-          </div>
-          <div class="flex flex-col space-y-2">
-            <p class="flex justify-center">Created On</p>
-            <div class="flex flex-col items-center text-xl space-y-8">
-              <p>15 Aug 2023 08:00</p>
-              <p>15 Aug 2023 08:00</p>
-            </div>
-          </div>
-          <div class="flex flex-col space-y-2">
-            <p class="flex justify-center">Updated On</p>
-            <div class="flex flex-col items-center text-xl space-y-8">
-              <p>15 Aug 2023 08:00</p>
-              <p>15 Aug 2023 08:00</p>
-            </div>
-          </div>
-          <div class="flex flex-col space-y-2">
-            <p class="flex justify-center">Action</p>
-            <div class="flex flex-col items-center text-xl space-y-8">
-              <div class="flex flex-row space-x-3">
-                <button class="px-4 py-1 rounded-md bg-gray-50 dark:bg-gray-700">edit</button>
-                <button class="px-4 py- rounded-md bg-gray-50 dark:bg-gray-700 ">delete</button>
-              </div>
-              <div class="flex flex-row space-x-3">
-                <button class="px-4 py-1 rounded-md bg-gray-50 dark:bg-gray-700">edit</button>
-                <button class="px-4 py- rounded-md bg-gray-50 dark:bg-gray-700 ">delete</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <userBox></userBox>
       </div>
     </format_page>
   </div>
