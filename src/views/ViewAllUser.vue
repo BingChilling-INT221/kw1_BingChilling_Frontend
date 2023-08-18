f724eba45bb308823ff9b707edccad477d861f81
Lahirerr <m_tanachart@hotmail.com>
2023-08-18 21:21:39 +0700

    Create ViewAllUser.vue

---
+++ src/views/ViewAllUser.vue
@@ -0,0 +1,16 @@
+<script setup>
+import format_page from "../components/format_page.vue";
+</script>
+ 
+<template>
+<format_page>
+    <div>
+                  
+    </div>
+</format_page>
+
+</template>
+ 
+<style scoped>
+
+</style>
