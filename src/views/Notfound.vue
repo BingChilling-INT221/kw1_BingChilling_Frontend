4a34fa9c28c69216498be53abdd6c70216dcc1a0
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-09-16 19:33:40 +0700

    improve not found

--- src/views/Notfound.vue
+++ src/views/Notfound.vue
@@ -1,18 +1,17 @@
-<script setup></script>
-
 <template>
-  <div class="inset-0 min-h-screen  bg-whiteCustom dark:bg-blackCustom ">
-    <div class="flex flex-col p-3 m-auto">
-      <p class="text-black">NOT FOUND!!!</p>
+  <div class="inset-0 min-h-screen bg-whiteCustom dark:bg-blackCustom">
+    <div class="flex flex-col items-center justify-center h-full p-3 m-auto">
+      <h1 class="mb-4 text-4xl font-bold">Page Not Found</h1>
+      <p class="mb-6 text-lg">The page you are looking for does not exist.</p>
       <button
-        class="text-green-500"
-        @click="$router.push({ name: 'userhomepage' })"
+        class="px-2 py-4 m-4 text-white rounded-xl bg-greenCustom"
+        @click="goTo('userhomepage')"
       >
         Go to Homepage
       </button>
       <button
-        class="text-red-500"
-        @click="$router.push({ name: 'adminhomepage' })"
+        class="px-2 py-4 m-4 text-white rounded-xl bg-redCustom"
+        @click="goTo('adminhomepage')"
       >
         Go to Admin Homepage
       </button>
@@ -20,4 +19,42 @@
   </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { useRouter } from "vue-router";
+
+const { push } = useRouter();
+
+function goTo(routeName) {
+  try {
+    push({ name: routeName });
+  } catch (error) {
+    console.error("Error navigating:", error);
+  }
+}
+</script>
+
+<style scoped>
+/* .bg-whiteCustom {
+  background-color: #ffffff;
+}
+
+.bg-blackCustom {
+  background-color: #000000;
+}
+
+.btn {
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  margin: 0.5rem;
+}
+
+.btn-green {
+  background-color: #43d482;
+  color: #ffffff;
+}
+
+.btn-red {
+  background-color: #ef4444;
+  color: #ffffff;
+} */
+</style>
