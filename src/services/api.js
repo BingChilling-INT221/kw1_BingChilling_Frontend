3554fc6f9e46fbf88cb07cfd2b688ec2ab5c55f0
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-09-27 22:25:45 +0700

    change   accesstoken refreshtoken

--- src/services/api.js
+++ src/services/api.js
@@ -1,204 +1,201 @@
-const accesstoken = localStorage.getItem('accesstoken');
-const refreshtoken = localStorage.getItem('refreshtoken');
-export const fetched_api = async ( role,category, mode, page, pageSize) => {
-    console.log("fetched_api")
-    try {
-      const selectCategory = (() => {
-        return category !== "" ? `&category=${category}` : "";
-      });
-      const modeFetch = (() => {
-        if(role === "admin")
-        {
-          return ""
-        }
-        return mode !== "" ? `&mode=${mode}` : "";
-      });
-      console.log(accesstoken)
-      return await fetch(
-        `${import.meta.env.VITE_BASE_URL}announcements/pages?${modeFetch()}&page=${page}&size=${pageSize}${selectCategory()}`,
-        {
-          headers: {
-            Authorization: `${accesstoken}`,
-          },
-        }
-      );
-         
-    } catch (err) {
-        console.log(err);
-      alert("ยังหาข้อมูลไม่พบโปรดรีเฟรชหน้าอีกครั้งครับ");
-      // window.location.reload();
-    }
-  };
-
-
-  export const fetchCate = async() =>{
-    try {
-      console.log(accesstoken)
-      const response = await fetch(`${import.meta.env.VITE_BASE_URL}categories`,
+export const fetched_api = async (role, category, mode, page, pageSize) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  console.log("fetched_api");
+  try {
+    const selectCategory = () => {
+      return category !== "" ? `&category=${category}` : "";
+    };
+    const modeFetch = () => {
+      if (role === "admin") {
+        return "";
+      }
+      return mode !== "" ? `&mode=${mode}` : "";
+    };
+    console.log(accesstoken);
+    return await fetch(
+      `${
+        import.meta.env.VITE_BASE_URL
+      }announcements/pages?${modeFetch()}&page=${page}&size=${pageSize}${selectCategory()}`,
       {
         headers: {
           Authorization: `${accesstoken}`,
         },
-      });
-      if (response.status === 200) {
-        return await response.json();
-      } else {
-        const errorResponse = await response.json();
-        throw new Error(errorResponse.message);
       }
-    } catch (err) {
-      throw err;
-    }
+    );
+  } catch (err) {
+    console.log(err);
+    alert("ยังหาข้อมูลไม่พบโปรดรีเฟรชหน้าอีกครั้งครับ");
+    // window.location.reload();
   }
+};
 
-  export const fetchCountParam = async(route,count) =>{
-        return await fetch(
-          `${import.meta.env.VITE_BASE_URL}announcements/${route}?count=${count}`,
-          {
-            headers: {
-              Authorization: `${accesstoken}`,
-            },
-          })
-  }
-
-
-  export const fetchCateForMod = async() =>{
-    return fetch(`${import.meta.env.VITE_BASE_URL}categories`,
-    {
+export const fetchCate = async () => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  try {
+    console.log(accesstoken);
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}categories`, {
       headers: {
         Authorization: `${accesstoken}`,
       },
     });
+    if (response.status === 200) {
+      return await response.json();
+    } else {
+      const errorResponse = await response.json();
+      throw new Error(errorResponse.message);
+    }
+  } catch (err) {
+    throw err;
   }
+};
 
-  export const fetchCreate = async(sendPackage) =>{
-    return await fetch(
-      `${import.meta.env.VITE_BASE_URL}announcements`,
-      {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(sendPackage),
-      }
-  );
-  }
-
-  export const fetchCreateUser = async(sendPackage) =>{
-    return await fetch(
-      `${import.meta.env.VITE_BASE_URL}users`,
-      {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(sendPackage),
-      }
-  );
-  }
-
-  export const fetchUpdate = async (sendPackage, route) =>{
-    return await fetch(
-      `${import.meta.env.VITE_BASE_URL}announcements/${route.params.id}`,
-      {
-          method: "PUT",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(sendPackage),
-      }
-  );
-  }
-
-  export const fetchUpdateUser = async (sendPackage, route) =>{
-    return await fetch(
-      `${import.meta.env.VITE_BASE_URL}users/${route.params.id}`,
-      {
-          method: "PUT",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(sendPackage),
-      }
-  );
-  }
-
-
-  export const fetchShowEdit = async(route) =>{
-    return await fetch(
-      `${import.meta.env.VITE_BASE_URL}announcements/${route}`
-  );
-  }
-
-
-  export const fetchDelete = async (id) =>{
-    return await fetch(
-      `${import.meta.env.VITE_BASE_URL}announcements/${id}`,
-      {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      }
-    );
-  }
-
-
-  export const fetchDeleteUser = async (id) =>{
-    return await fetch(
-      `${import.meta.env.VITE_BASE_URL}users/${id}`,
-      {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      }
-    );
-  }
-
-
-  export const fetchUser = async () =>{
-    return await fetch(
-      `${import.meta.env.VITE_BASE_URL}users`,{
-        headers: {
-          Authorization: `${accesstoken}`,
-        },
-      }
-      
-  );
-  }
-
-  export const fetchUserEdit = async(route) =>{
-    return await fetch(
-      `${import.meta.env.VITE_BASE_URL}users/${route}`,{
-        headers: {
-          Authorization: `${accesstoken}`,
-        },
-      }
-  );
-  }
-
-  export const fetchMatch = async (sendData) =>{
-    return await fetch(`${import.meta.env.VITE_BASE_URL}users/match`,
+export const fetchCountParam = async (route, count) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(
+    `${import.meta.env.VITE_BASE_URL}announcements/${route}?count=${count}`,
     {
-      method: "POST",
       headers: {
-          "Content-Type": "application/json"
+        Authorization: `${accesstoken}`,
       },
-      body: JSON.stringify(sendData),
-  })
-  }
-  export const fetchCreateToken = async (sendData) =>{
-    return await fetch(`${import.meta.env.VITE_BASE_URL}token`,
+    }
+  );
+};
+
+export const fetchCateForMod = async () => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return fetch(`${import.meta.env.VITE_BASE_URL}categories`, {
+    headers: {
+      Authorization: `${accesstoken}`,
+    },
+  });
+};
+
+export const fetchCreate = async (sendPackage) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(`${import.meta.env.VITE_BASE_URL}announcements`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(sendPackage),
+  });
+};
+
+export const fetchCreateUser = async (sendPackage) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(`${import.meta.env.VITE_BASE_URL}users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(sendPackage),
+  });
+};
+
+export const fetchUpdate = async (sendPackage, route) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(
+    `${import.meta.env.VITE_BASE_URL}announcements/${route.params.id}`,
     {
-      method: "POST",
+      method: "PUT",
       headers: {
-          "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(sendData),
-  })
-  }
-  // export default fetched_api;
+      body: JSON.stringify(sendPackage),
+    }
+  );
+};
 
+export const fetchUpdateUser = async (sendPackage, route) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(
+    `${import.meta.env.VITE_BASE_URL}users/${route.params.id}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(sendPackage),
+    }
+  );
+};
 
+export const fetchShowEdit = async (route) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(`${import.meta.env.VITE_BASE_URL}announcements/${route}`);
+};
 
+export const fetchDelete = async (id) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(`${import.meta.env.VITE_BASE_URL}announcements/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+export const fetchDeleteUser = async (id) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(`${import.meta.env.VITE_BASE_URL}users/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+export const fetchUser = async () => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(`${import.meta.env.VITE_BASE_URL}users`, {
+    headers: {
+      Authorization: `${accesstoken}`,
+    },
+  });
+};
+
+export const fetchUserEdit = async (route) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(`${import.meta.env.VITE_BASE_URL}users/${route}`, {
+    headers: {
+      Authorization: `${accesstoken}`,
+    },
+  });
+};
+
+export const fetchMatch = async (sendData) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(`${import.meta.env.VITE_BASE_URL}users/match`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(sendData),
+  });
+};
+export const fetchCreateToken = async (sendData) => {
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
+  return await fetch(`${import.meta.env.VITE_BASE_URL}token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(sendData),
+  });
+};
+// export default fetched_api;
