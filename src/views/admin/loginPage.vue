197d50830583539c841e51dc508314d55e0a9562
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-09-27 22:38:28 +0700

    comant

--- src/views/admin/loginPage.vue
+++ src/views/admin/loginPage.vue
@@ -7,7 +7,7 @@ const username = ref("");
 const password = ref("");
 
 const status = ref("");
-const rou= useRouter();
+const rou = useRouter();
 const accesstoken = ref("");
 const refreshtoken = ref("");
 const sendSubmit = async (event) => {
@@ -19,27 +19,25 @@ const sendSubmit = async (event) => {
   try {
     const response = await fetchCreateToken(sendData);
     if (response.status === 200) {
-      status.value = 200
-      const result = await response.json()
-      accesstoken.value = result.accesstoken
-      refreshtoken.value = result.refreshtoken
-      console.log("hi", accesstoken.value,"\n", refreshtoken.value)
-      localStorage.setItem('accesstoken', `Bearer ${accesstoken.value}`);
-      localStorage.setItem('refreshtoken',`Bearer ${refreshtoken.value}`);
-      rou.push({name: "adminhomepage"} );
+      status.value = 200;
+      const result = await response.json();
+      accesstoken.value = result.accesstoken;
+      refreshtoken.value = result.refreshtoken;
+      console.log("hi", accesstoken.value, "\n", refreshtoken.value);
+      localStorage.setItem("accesstoken", `Bearer ${accesstoken.value}`);
+      localStorage.setItem("refreshtoken", `Bearer ${refreshtoken.value}`);
+      rou.push({ name: "adminhomepage" });
     } else if (response.status === 401) {
-      status.value = 401
-      console.log("hi2", response)
-    }
-    else if (response.status === 404) {
-      status.value = 404
-      console.log("hi3", response)
-    }
-    else {
-      console.log("hihi", response)
+      status.value = 401;
+      console.log("hi2", response);
+    } else if (response.status === 404) {
+      status.value = 404;
+      console.log("hi3", response);
+    } else {
+      console.log("hihi", response);
     }
   } catch (err) {
-    console.log("hi4", response)
+    console.log("hi4", response);
     alert(err);
   }
 };
@@ -48,55 +46,73 @@ provide(/* key */ "role", /* value */ "admin");
 </script>
 
 <template>
-
   <div class="w-full">
     <!-- <format_page> -->
 
-      <form class="w-full" @submit="sendSubmit">
-        <div class="flex flex-col items-center pt-20 space-y-5">
-          <div class="flex w-3/4 ann-message">
-            <div v-if="status == 200" class="p-6 text-green-500 bg-green-200 border-green-500 rounded-xl">
-              <p class="text-2xl">Password Matched</p>
-            </div>
-            <div v-if="status == 401" class="p-6 text-red-500 bg-red-200 border-red-500 rounded-xl">
-              <p class="text-2xl">Password NOT Matched</p>
-            </div>
-            <div v-if="status == 404" class="p-6 text-red-500 bg-red-200 border-red-500 rounded-xl">
-              <p class="text-2xl">The specified username DOES NOT exist</p>
-            </div>
+    <form class="w-full" @submit="sendSubmit">
+      <div class="flex flex-col items-center pt-20 space-y-5">
+        <div class="flex w-3/4 ann-message">
+          <div
+            v-if="status == 200"
+            class="p-6 text-green-500 bg-green-200 border-green-500 rounded-xl"
+          >
+            <p class="text-2xl">Password Matched</p>
           </div>
+          <div
+            v-if="status == 401"
+            class="p-6 text-red-500 bg-red-200 border-red-500 rounded-xl"
+          >
+            <p class="text-2xl">Password NOT Matched</p>
+          </div>
+          <div
+            v-if="status == 404"
+            class="p-6 text-red-500 bg-red-200 border-red-500 rounded-xl"
+          >
+            <p class="text-2xl">The specified username DOES NOT exist</p>
+          </div>
+        </div>
 
-          <div class="flex flex-row w-3/4 rounded-lg">
-            <div class="flex flex-col w-3/4 p-5 space-y-5 border-2 rounded-md">
-              <div class="flex text-4xl">
-                <p>Match password</p>
+        <div class="flex flex-row w-3/4 rounded-lg">
+          <div class="flex flex-col w-3/4 p-5 space-y-5 border-2 rounded-md">
+            <div class="flex text-4xl">
+              <p>Match password</p>
+            </div>
+            <div class="flex flex-col space-y-2 text-lg">
+              <div class="flex flex-row w-3/4">
+                <p>Username</p>
               </div>
-              <div class="flex flex-col space-y-2 text-lg">
-                <div class="flex flex-row w-3/4">
-                  <p>Username</p>
-                </div>
-                <input v-model="username"
-                  class="w-3/4 px-2 py-2 rounded-md ann-title bg-whitesecondCustom dark:bg-darksecondCustom ann-username"
-                  type="text" maxlength="45" required />
+              <input
+                v-model="username"
+                class="w-3/4 px-2 py-2 rounded-md ann-title bg-whitesecondCustom dark:bg-darksecondCustom ann-username"
+                type="text"
+                maxlength="45"
+                required
+              />
+            </div>
+            <div class="flex flex-col space-y-2 text-lg">
+              <div class="flex flex-row w-3/4">
+                <p>Password</p>
               </div>
-              <div class="flex flex-col space-y-2 text-lg">
-                <div class="flex flex-row w-3/4">
-                  <p>Password</p>
-                </div>
-                <input v-model="password"
-                  class="w-3/4 px-2 py-2 rounded-md ann-title bg-whitesecondCustom dark:bg-darksecondCustom ann-password"
-                  type="password" maxlength="14" required />
-              </div>
-              <div class="flex">
-                <button class="px-4 py-2 rounded-md ann-button bg-gray-50 dark:bg-gray-700 submit">
-                  Go go go Ranger
-                </button>
-              </div>
+              <input
+                v-model="password"
+                class="w-3/4 px-2 py-2 rounded-md ann-title bg-whitesecondCustom dark:bg-darksecondCustom ann-password"
+                type="password"
+                maxlength="14"
+                required
+              />
+            </div>
+            <div class="flex">
+              <button
+                class="px-4 py-2 rounded-md ann-button bg-gray-50 dark:bg-gray-700 submit"
+              >
+                Go go go Ranger
+              </button>
             </div>
           </div>
         </div>
-      </form>
-    </format_page>
+      </div>
+    </form>
+    <!-- </format_page> -->
   </div>
 </template>
 
