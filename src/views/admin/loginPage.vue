fdcdd9d516152652622d9079bb3dde3c4cd2a636
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-09-27 21:45:52 +0700

    Revert "Revert "add login""
    This reverts commit d6f94327a081065a6379da8eb7c553d408707492.

---
+++ src/views/admin/loginPage.vue
@@ -0,0 +1,105 @@
+<script setup>
+import { provide, ref, onMounted } from "vue";
+import format_page from "@/components/format_page.vue";
+import { fetchCreateToken } from "../../services/api.js";
+import { useRouter } from "vue-router";
+const username = ref("");
+const password = ref("");
+
+const status = ref("");
+const rou= useRouter();
+const accesstoken = ref("");
+const refreshtoken = ref("");
+const sendSubmit = async (event) => {
+  event.preventDefault();
+  const sendData = {
+    username: username.value,
+    password: password.value,
+  };
+  try {
+    const response = await fetchCreateToken(sendData);
+    if (response.status === 200) {
+      status.value = 200
+      const result = await response.json()
+      accesstoken.value = result.accesstoken
+      refreshtoken.value = result.refreshtoken
+      console.log("hi", accesstoken.value,"\n", refreshtoken.value)
+      localStorage.setItem('accesstoken', `Bearer ${accesstoken.value}`);
+      localStorage.setItem('refreshtoken',`Bearer ${refreshtoken.value}`);
+      rou.push({name: "adminhomepage"} );
+    } else if (response.status === 401) {
+      status.value = 401
+      console.log("hi2", response)
+    }
+    else if (response.status === 404) {
+      status.value = 404
+      console.log("hi3", response)
+    }
+    else {
+      console.log("hihi", response)
+    }
+  } catch (err) {
+    console.log("hi4", response)
+    alert(err);
+  }
+};
+
+provide(/* key */ "role", /* value */ "admin");
+</script>
+
+<template>
+  {{ accesstoken }}
+  <br>
+  {{ refreshtoken }}
+  <div class="w-full">
+    <format_page>
+
+      <form class="w-full" @submit="sendSubmit">
+        <div class="flex flex-col space-y-5 items-center pt-20">
+          <div class="flex w-3/4 ann-message">
+            <div v-if="status == 200" class="p-6  text-green-500 bg-green-200 border-green-500 rounded-xl">
+              <p class="text-2xl">Password Matched</p>
+            </div>
+            <div v-if="status == 401" class="p-6 text-red-500 bg-red-200 border-red-500 rounded-xl">
+              <p class="text-2xl">Password NOT Matched</p>
+            </div>
+            <div v-if="status == 404" class="p-6  text-red-500 bg-red-200 border-red-500 rounded-xl">
+              <p class="text-2xl">The specified username DOES NOT exist</p>
+            </div>
+          </div>
+
+          <div class="flex flex-row rounded-lg w-3/4">
+            <div class="flex flex-col space-y-5 w-3/4 p-5 border-2 rounded-md">
+              <div class="flex text-4xl">
+                <p>Match password</p>
+              </div>
+              <div class="text-lg flex flex-col space-y-2">
+                <div class="flex flex-row w-3/4">
+                  <p>Username</p>
+                </div>
+                <input v-model="username"
+                  class="rounded-md w-3/4 ann-title bg-whitesecondCustom dark:bg-darksecondCustom py-2 px-2 ann-username"
+                  type="text" maxlength="45" required />
+              </div>
+              <div class="text-lg flex flex-col space-y-2">
+                <div class="flex flex-row w-3/4">
+                  <p>Password</p>
+                </div>
+                <input v-model="password"
+                  class="rounded-md w-3/4 ann-title bg-whitesecondCustom dark:bg-darksecondCustom py-2 px-2 ann-password"
+                  type="password" maxlength="14" required />
+              </div>
+              <div class="flex">
+                <button class="ann-button px-4 py-2 rounded-md bg-gray-50 dark:bg-gray-700 submit">
+                  Go go go Ranger
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </format_page>
+  </div>
+</template>
+
+<style scoped></style>
