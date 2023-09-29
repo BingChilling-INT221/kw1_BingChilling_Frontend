2ade26df8083e28a6d0b1262a5f7bf112d967eaf
Lahirerr <m_tanachart@hotmail.com>
2023-09-29 21:03:40 +0700

    finish log out

--- src/services/api.js
+++ src/services/api.js
@@ -182,7 +182,7 @@ export const fetchMatch = async (sendData) => {
     return await fetch(`${import.meta.env.VITE_BASE_URL}users/match`, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+            Authorization: `${token}`,
         },
         body: JSON.stringify(sendData),
     });
@@ -193,11 +193,22 @@ export const fetchCreateToken = async (sendData) => {
     return await fetch(`${import.meta.env.VITE_BASE_URL}token`, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+            Authorization: `${token}`,
         },
         body: JSON.stringify(sendData),
     });
 };
+
+export const reToken = async () =>{
+    const refreshToken = localStorage.getItem("refreshToken");
+
+    return await fetch(`${import.meta.env.VITE_BASE_URL}token`, {
+        method: "GET",
+        headers: {
+            Authorization: `${refreshToken}`,
+        },
+    })
+}
 
 
 // export default fetched_api;
