074c68049354ce4b03e9d51e1342fdefb9e05174
Lahirerr <m_tanachart@hotmail.com>
2023-09-29 18:07:37 +0700

    add onMounted

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {inject,ref} from "vue";
+import {inject,ref,onMounted} from "vue";
 import DarkLightIcon from "./DarkLightIcon.vue";
 import Menu from "../icons/Menu.vue";
 import SearchBox from "../SearchBox.vue";
