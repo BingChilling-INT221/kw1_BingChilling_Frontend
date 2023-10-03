52202ec82f0037d0ca59735f329b4f7d95c93544
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-10-03 22:16:47 +0700

    change style

--- src/router/index.js
+++ src/router/index.js
@@ -1,91 +1,75 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import AddAnnPage from '../views/admin/AddAnnPage.vue'
-import EditAnnPage from '../views/admin/EditAnnPage.vue'
-import AnnPage from '../views/users/AnnPage.vue'
-import AnnDetailPage from '../views/users/AnnDetailPage.vue'
-import AdminAnnPage from '../views/admin/AdminAnnPage.vue'
-import AdminAnnDetail from '../views/admin/AdminAnnDetailPage.vue'
-import Notfound from "@/views/Notfound.vue";
-import UsersPage from "@/views/admin/UsersPage.vue";
-import AddUserPage from "@/views/admin/AddUserPage.vue";
-import EditUserPage from "@/views/admin/EditUserPage.vue";
-import MatchPassword from "@/views/admin/MatchPassword.vue";
-import Login from "@/views/admin/loginPage.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Notfound',
+        component: () => import('@/views/Notfound.vue')
+    },
+    {
+        path: '/admin/announcement',
+        name: 'adminhomepage',
+        component: () => import('@/views/admin/AdminAnnPage.vue')
+    },
+    {
+        path: '/admin/announcement/:id',
+        name: 'adminannouncementdetail',
+        component: () => import('@/views/admin/AdminAnnDetailPage.vue')
+    },
+    {
+        path: '/admin/announcement/add',
+        name: 'addannouncement',
+        component: () => import('@/views/admin/AddAnnPage.vue')
+    },
+    {
+        path: '/admin/announcement/:id/edit',
+        name: 'editannouncement',
+        component: () => import('@/views/admin/EditAnnPage.vue')
+    },
+    {
+        path: '/announcement',
+        name: 'userhomepage',
+        component: () => import('@/views/users/AnnPage.vue')
+    },
+    {
+        path: '/announcement/:id',
+        name: 'userannouncementdetail',
+        component: () => import('@/views/users/AnnDetailPage.vue')
+    },
+    {
+        path: '/admin/user',
+        name: 'adminuserpage',
+        component: () => import('@/views/admin/UsersPage.vue')
+    },
+    {
+        path: '/admin/user/add',
+        name: 'adduser',
+        component: () => import('@/views/admin/AddUserPage.vue')
+    },
+    {
+        path: '/admin/user/:id/edit',
+        name: 'edituser',
+        component: () => import('@/views/admin/EditUserPage.vue')
+    },
+    {
+        path: '/admin/user/match',
+        name: 'matchpassword',
+        component: () => import('@/views/admin/MatchPassword.vue')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/admin/loginPage.vue')
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        component: () => import('@/views/Notfound.vue')
+    }
+]
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'Notfound',
-            component: Notfound
-        },
-        {
-            path: '/admin/announcement',
-            name: 'adminhomepage',
-            component: AdminAnnPage,
-        },
-        {
-            path: '/admin/announcement/:id',
-            name: 'adminannouncementdetail',
-            component: AdminAnnDetail,
-        },
-        {
-            path: '/admin/announcement/add',
-            name: 'addannouncement',
-            component: AddAnnPage
-        },
-        {
-            path: '/admin/announcement/:id/edit',
-            name: 'editannouncement',
-            component: EditAnnPage,
-        },
-        {
-            path: '/announcement',
-            name: 'userhomepage',
-            component: AnnPage
-        },
-        {
-            path: '/announcement/:id',
-            name: 'userannouncementdetail',
-            component: AnnDetailPage
-        },
-        {
-            path: '/admin/user',
-            name: 'adminuserpage',
-            component: UsersPage
-        },
-        // {
-        //     path: "/:pathMatch(.*)*",
-        //     component: Notfound,
-        // }
-        {
-            path: '/admin/user/add',
-            name: 'adduser',
-            component: AddUserPage
-        },
-        {
-            path: '/admin/user/:id/edit',
-            name: 'edituser',
-            component: EditUserPage,
-        },
-        {
-            path: '/admin/user/match',
-            name: 'matchpassword',
-            component: MatchPassword,
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: Login,
-        },
-        {
-            path: "/:pathMatch(.*)*",
-            component: Notfound,
-        }
-
-
-    ]
+    routes
 })
 
 export default router
