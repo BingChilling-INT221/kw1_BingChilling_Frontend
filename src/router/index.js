914518fd551a77f2ee6cb7b5265a606ff30736a4
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-11-12 15:42:15 +0700

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -1,147 +1,145 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import {useUsersStore} from "@/stores/user";
+import { createRouter, createWebHistory } from "vue-router";
+import { useUsersStore } from "@/stores/user";
 
 const routes = [
-    {
-        path: '/',
-        name: 'mainpage',
-        redirect: '/announcement'
-    },
-    {
-        path: '/admin',
-        name: 'admin',
-        meta: {requiredRole: ['admin', 'announcer']},
-        children: [
-            {
-                path: 'announcement',
-                name: 'adminhomepage',
-                component: () => import('@/views/admin/AdminAnnPage.vue')
-            },
-            {
-                path: 'announcement/:id',
-                name: 'adminannouncementdetail',
-                component: () => import('@/views/admin/AdminAnnDetailPage.vue')
-            },
-            {
-                path: 'announcement/add',
-                name: 'addannouncement',
-                component: () => import('@/views/admin/AddAnnPage.vue')
-            },
-            {
-                path: 'announcement/:id/edit',
-                name: 'editannouncement',
-                component: () => import('@/views/admin/EditAnnPage.vue')
-            },
-            {
-                path: 'user',
-                name: 'adminuserpage',
-                component: () => import('@/views/admin/UsersPage.vue')
-            },
-            {
-                path: 'user/add',
-                name: 'adduser',
-                component: () => import('@/views/admin/AddUserPage.vue')
-            },
-            {
-                path: 'user/:id/edit',
-                name: 'edituser',
-                component: () => import('@/views/admin/EditUserPage.vue')
-            },
-            {
-                path: 'user/match',
-                name: 'matchpassword',
-                component: () => import('@/views/admin/MatchPassword.vue')
-            },
-            {
-                path: 'announcement/viewer',
-                name: 'viewer',
-                component: () => import('@/views/admin/ViewerAnnPage.vue')
-            },
-            {
-                path: 'announcement/viewer/:id',
-                name: 'viewerannouncementdetail',
-                component: () => import('@/views/users/AnnDetailPage.vue')
-            }
-
-        ]
-    },
-    // {
-    //     path: '/admin/announcement',
-    //     name: 'adminhomepage',
-    //     meta: {requiredRole: ['admin','announcer']},
-    //     component: () => import('@/views/admin/AdminAnnPage.vue')
-    // },
-    // {
-    //     path: '/admin/announcement/:id',
-    //     name: 'adminannouncementdetail',
-    //     component: () => import('@/views/admin/AdminAnnDetailPage.vue')
-    // },
-    // {
-    //     path: '/admin/announcement/add',
-    //     name: 'addannouncement',
-    //     component: () => import('@/views/admin/AddAnnPage.vue')
-    // },
-    // {
-    //     path: '/admin/announcement/:id/edit',
-    //     name: 'editannouncement',
-    //     component: () => import('@/views/admin/EditAnnPage.vue')
-    // },
-    {
-        path: '/announcement',
-        name: 'userhomepage',
-        component: () => import('@/views/users/AnnPage.vue')
-    },
-    {
-        path: '/announcement/:id',
-        name: 'userannouncementdetail',
-        component: () => import('@/views/users/AnnDetailPage.vue')
-    },
-    // {
-    //     path: '/admin/user',
-    //     name: 'adminuserpage',
-    //     component: () => import('@/views/admin/UsersPage.vue')
-    // },
-    // {
-    //     path: '/admin/user/add',
-    //     name: 'adduser',
-    //     component: () => import('@/views/admin/AddUserPage.vue')
-    // },
-    // {
-    //     path: '/admin/user/:id/edit',
-    //     name: 'edituser',
-    //     component: () => import('@/views/admin/EditUserPage.vue')
-    // },
-    // {
-    //     path: '/admin/user/match',
-    //     name: 'matchpassword',
-    //     component: () => import('@/views/admin/MatchPassword.vue')
-    // },
-    {
-        path: '/login',
-        name: 'login',
-        component: () => import('@/views/admin/loginPage.vue')
-    }, {
-        path: '/register',
-        name: 'register',
-        component: () => import('@/views/admin/registerView.vue')
-    },
-    {
-        path: '/403',
-        name: '403',
-        component: () => import('@/views/errorpage/403page.vue')
-    },
-    {
-        path: '/:pathMatch(.*)*',
-        component: () => import('@/views/errorpage/Notfound.vue')
-    }
-
-]
+  {
+    path: "/",
+    name: "mainpage",
+    redirect: "/announcement",
+  },
+  {
+    path: "/admin",
+    name: "admin",
+    meta: { requiredRole: ["admin", "announcer"] },
+    children: [
+      {
+        path: "announcement",
+        name: "adminhomepage",
+        component: () => import("@/views/admin/AdminAnnPage.vue"),
+      },
+      {
+        path: "announcement/:id",
+        name: "adminannouncementdetail",
+        component: () => import("@/views/admin/AdminAnnDetailPage.vue"),
+      },
+      {
+        path: "announcement/add",
+        name: "addannouncement",
+        component: () => import("@/views/admin/AddAnnPage.vue"),
+      },
+      {
+        path: "announcement/:id/edit",
+        name: "editannouncement",
+        component: () => import("@/views/admin/EditAnnPage.vue"),
+      },
+      {
+        path: "user",
+        name: "adminuserpage",
+        component: () => import("@/views/admin/UsersPage.vue"),
+      },
+      {
+        path: "user/add",
+        name: "adduser",
+        component: () => import("@/views/admin/AddUserPage.vue"),
+      },
+      {
+        path: "user/:id/edit",
+        name: "edituser",
+        component: () => import("@/views/admin/EditUserPage.vue"),
+      },
+      {
+        path: "user/match",
+        name: "matchpassword",
+        component: () => import("@/views/admin/MatchPassword.vue"),
+      },
+      {
+        path: "announcement/viewer",
+        name: "viewer",
+        component: () => import("@/views/admin/ViewerAnnPage.vue"),
+      },
+      {
+        path: "announcement/viewer/:id",
+        name: "viewerannouncementdetail",
+        component: () => import("@/views/users/AnnDetailPage.vue"),
+      },
+    ],
+  },
+  // {
+  //     path: '/admin/announcement',
+  //     name: 'adminhomepage',
+  //     meta: {requiredRole: ['admin','announcer']},
+  //     component: () => import('@/views/admin/AdminAnnPage.vue')
+  // },
+  // {
+  //     path: '/admin/announcement/:id',
+  //     name: 'adminannouncementdetail',
+  //     component: () => import('@/views/admin/AdminAnnDetailPage.vue')
+  // },
+  // {
+  //     path: '/admin/announcement/add',
+  //     name: 'addannouncement',
+  //     component: () => import('@/views/admin/AddAnnPage.vue')
+  // },
+  // {
+  //     path: '/admin/announcement/:id/edit',
+  //     name: 'editannouncement',
+  //     component: () => import('@/views/admin/EditAnnPage.vue')
+  // },
+  {
+    path: "/announcement",
+    name: "userhomepage",
+    component: () => import("@/views/users/AnnPage.vue"),
+  },
+  {
+    path: "/announcement/:id",
+    name: "userannouncementdetail",
+    component: () => import("@/views/users/AnnDetailPage.vue"),
+  },
+  // {
+  //     path: '/admin/user',
+  //     name: 'adminuserpage',
+  //     component: () => import('@/views/admin/UsersPage.vue')
+  // },
+  // {
+  //     path: '/admin/user/add',
+  //     name: 'adduser',
+  //     component: () => import('@/views/admin/AddUserPage.vue')
+  // },
+  // {
+  //     path: '/admin/user/:id/edit',
+  //     name: 'edituser',
+  //     component: () => import('@/views/admin/EditUserPage.vue')
+  // },
+  // {
+  //     path: '/admin/user/match',
+  //     name: 'matchpassword',
+  //     component: () => import('@/views/admin/MatchPassword.vue')
+  // },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/admin/loginPage.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("@/views/admin/registerView.vue"),
+  },
+  {
+    path: "/403",
+    name: "403",
+    component: () => import("@/views/errorpage/403page.vue"),
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    component: () => import("@/views/errorpage/Notfound.vue"),
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes
-})
-
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
 
 // router.beforeEach((to, from, next) => {
 //     const requiredRole = to.meta.requiredRole;
@@ -175,43 +173,85 @@ const router = createRouter({
 //         next('/login');
 //     }
 // });
+// router.beforeEach(async (to, from, next) => {
+//   const requiredRole = to.meta.requiredRole;
+//   const usersStore = useUsersStore();
+//   let token = usersStore.token;
+//   if (token === null || token === "") {
+//     await usersStore.recall();
+//     token = usersStore.token;
+//     if (token === null || token === "") {
+//       next("/login");
+//       console.log("6");
+//     }
+//     console.log(usersStore.token, "tokenUser");
+//     console.log(usersStore.role, "roleUser");
+//   }
+//   const role = usersStore.role;
+//   console.log(role, "role");
+//   // If the user's token is empty and there are tokens in localStorage, recall the user's data.
+//   if (
+//     !token &&
+//     (localStorage.getItem("token") || localStorage.getItem("refreshToken"))
+//   ) {
+//     await usersStore.recall();
+//   }
+
+//   // If the user is trying to visit the login page and they are already authenticated, redirect them.
+//   if (to.path === "/login") {
+//     if (token) {
+//       next("/admin/announcement");
+//       console.log("1");
+//       console.log(role);
+//     } else {
+//       next();
+//       console.log("2");
+//     }
+//   } else if (requiredRole === undefined) {
+//     // If no specific role is required, allow access to the route.
+//     next();
+//     console.log("3");
+//   } else if (requiredRole.includes(role)) {
+//     // If the user's role matches the required role, allow access to the route.
+//     next();
+//     console.log("4");
+//   } else {
+//     console.log(requiredRole, "requiredRole");
+//     console.log(role, "role");
+//     // If none of the conditions match, allow access to the route.
+//     next("/login");
+//     console.log("5");
+//     // console.log(role);
+//   }
+// });
+// export default router;
 router.beforeEach(async (to, from, next) => {
     const requiredRole = to.meta.requiredRole;
     const usersStore = useUsersStore();
-    const token = usersStore.token;
-    const role = usersStore.role;
-
+    let token = usersStore.token;
     
     // If the user's token is empty and there are tokens in localStorage, recall the user's data.
-    if (!token && (localStorage.getItem('token') || localStorage.getItem('refreshToken'))) {
+    if (!token && (localStorage.getItem("token") || localStorage.getItem("refreshToken"))) {
         await usersStore.recall();
-
-    
+        token = usersStore.token;
     }
 
     // If the user is trying to visit the login page and they are already authenticated, redirect them.
-    if (to.path === '/login') {
+    if (to.path === "/login") {
         if (token) {
-            next('/admin/announcement');
-            console.log('1');
-            console.log(role);
+            next("/admin/announcement");
         } else {
             next();
-            console.log('2');
         }
     } else if (requiredRole === undefined) {
         // If no specific role is required, allow access to the route.
         next();
-        console.log('3');
-    } else if (requiredRole.includes(role)) {
+    } else if (token && requiredRole.includes(usersStore.role)) {
         // If the user's role matches the required role, allow access to the route.
         next();
-        console.log('4');
-    }  else {
-        // If none of the conditions match, allow access to the route.
-        next('/login');
-        console.log('5');
-        console.log(role);
+    } else {
+        next("/login");
     }
 });
-export default router
+
+export default router;
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,62 +1,62 @@
-import {ref} from 'vue'
-import {defineStore} from 'pinia'
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import jwtDecode from "jwt-decode";
+export const useUsersStore = defineStore("users", () => {
+  const username = ref("");
+  const role = ref("");
+  const token = ref("");
+  const refreshToken = ref("");
 
-export const useUsersStore = defineStore('users', () => {
-    const username = ref('')
-    const role = ref('')
-    const token = ref('')
-    const refreshToken = ref('')
+  function setUsername(setUsername) {
+    username.value = setUsername;
+  }
 
-    function setUsername(setUsername) {
-        username.value = setUsername
-    }
+  function setRole(setRole) {
+    role.value = setRole;
+  }
 
-    function setRole(setRole) {
-        role.value = setRole
-    }
+  function setToken(setToken) {
+    token.value = setToken;
+    localStorage.setItem("token", setToken);
+  }
 
-    function setToken(setToken) {
-        token.value = setToken
-        localStorage.setItem('token', setToken);
-    }
+  function setRefreshToken(setRefreshToken) {
+    refreshToken.value = setRefreshToken;
+  }
 
-    function setRefreshToken(setRefreshToken) {
-        refreshToken.value = setRefreshToken
-    }
+  function logout() {
+    username.value = "";
+    role.value = "";
+    token.value = "";
+    refreshToken.value = "";
+    localStorage.removeItem("token");
+    localStorage.removeItem("refreshToken");
+  }
 
-    function logout() {
-        username.value = ''
-        role.value = ''
-        token.value = ''
-        refreshToken.value = ''
-        localStorage.removeItem('token')
-        localStorage.removeItem('refreshToken')
-    }
-
-    function recall() {
-        // token.value = localStorage.getItem('token')
-        // refreshToken.value = localStorage.getItem('refreshToken')
-        const storedToken = localStorage.getItem('token');
-        const storedRefreshToken = localStorage.getItem('refreshToken');
-        if (token.value === null) {
+function recall() {
+    const storedToken = localStorage.getItem("token");
+    const storedRefreshToken = localStorage.getItem("refreshToken");
+    if (storedToken && storedRefreshToken) {
+        if (!token.value) {
             token.value = storedToken;
             refreshToken.value = storedRefreshToken;
-            // const data = JWTDecode(token.value)
-            // username.value = data.username
-            // role.value = data.role
+            const { username: storedUsername, role: storedRole } = jwtDecode(token.value);
+            username.value = storedUsername;
+            role.value = storedRole;
         }
     }
+}
 
-    return {
-        username,
-        role,
-        token,
-        refreshToken,
-        setUsername,
-        recall,
-        setRole,
-        setToken,
-        setRefreshToken,
-        logout
-    }
-})
+  return {
+    username,
+    role,
+    token,
+    refreshToken,
+    setUsername,
+    recall,
+    setRole,
+    setToken,
+    setRefreshToken,
+    logout,
+  };
+});
