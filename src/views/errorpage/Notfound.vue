2efecb2ac8c67a9f7ea93d1aaa1a2d4b7efe52d8
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-12-18 23:53:16 +0700

    แก้บัคเล็กๆน้อยๆ

--- src/views/errorpage/Notfound.vue
+++ src/views/errorpage/Notfound.vue
@@ -1,66 +1,26 @@
+<script setup>
+import {useRouter} from "vue-router";
+
+
+const router = useRouter();
+
+
+</script>
+
 <template>
-  <div class="inset-0 min-h-screen bg-whiteCustom dark:bg-blackCustom">
-    <div class="flex flex-col items-center justify-center h-full p-3 m-auto">
-      <h1 class="mb-4 text-4xl font-bold">Page Not Found</h1>
-      <p class="mb-6 text-lg">The page you are looking for does not exist.</p>
-      <button
-          class="px-2 py-4 m-4 text-white rounded-xl bg-greenCustom"
-          @click="goTo('userhomepage')"
-      >
-        Go to Homepage
-      </button>
-      <button
-          class="px-2 py-4 m-4 text-white rounded-xl bg-redCustom"
-          @click="goTo('adminhomepage')"
-      >
-        Go to Admin Homepage
-      </button>
-      <button
-          class="px-2 py-4 m-4 text-white rounded-xl bg-redCustom"
-          @click="goTo('login')"
-      >
-        Go to login
-      </button>
+  <div>
+    <div class="min-h-screen flex items-center justify-center">
+      <div class="text-center">
+        <h1 class="text-4xl font-bold mb-4">404 - Not found</h1>
+        <p class="text-gray-400 mb-4">Sorry, the page you are looking for does not exist.</p>
+        <button @click="$router.push({ name: 'adminhomepage' })" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ">
+          Go to Home Page
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup>
-import {useRouter} from "vue-router";
-
-const {push} = useRouter();
-
-function goTo(routeName) {
-  try {
-    push({name: routeName});
-  } catch (error) {
-    console.error("Error navigating:", error);
-  }
-}
-</script>
-
 <style scoped>
-/* .bg-whiteCustom {
-  background-color: #ffffff;
-}
 
-.bg-blackCustom {
-  background-color: #000000;
-}
-
-.btn {
-  padding: 0.5rem 1rem;
-  border-radius: 0.25rem;
-  margin: 0.5rem;
-}
-
-.btn-green {
-  background-color: #43d482;
-  color: #ffffff;
-}
-
-.btn-red {
-  background-color: #ef4444;
-  color: #ffffff;
-} */
 </style>
