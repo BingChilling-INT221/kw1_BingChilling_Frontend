72ed91629a7ad7966b3dc3b8026184d325caeb18
Lahirerr <m_tanachart@hotmail.com>
2023-12-18 23:19:22 +0700

    403 and fix detail

--- src/views/errorpage/403page.vue
+++ src/views/errorpage/403page.vue
@@ -9,8 +9,15 @@ const router = useRouter();
 
 <template>
   <div>
-    this is 403 page
-    <button @click="$router.push({ name: 'adminhomepage' })">กดสิ</button>
+    <div class="min-h-screen flex items-center justify-center">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold mb-4">403 - Forbidden</h1>
+      <p class="text-gray-400 mb-4">Sorry, you do not have permission to access this page.</p>
+      <button @click="$router.push({ name: 'adminhomepage' })" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ">
+        Go to Home Page
+      </button>
+    </div>
+  </div>
   </div>
 </template>
 
